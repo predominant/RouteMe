d35cfc86af70d165d6d3a5f156041d6db20a9b28
Graham Weldon <graham@grahamweldon.com>
2012-06-02 21:14:53 +1000

    Moved Server to ResponseServer

---
+++ RouteMeServer/src/routeme/server/response/ResponseServer.java
@@ -0,0 +1,13 @@
+package routeme.server.response;
+
+public class ResponseServer {
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ResponseServer server = new ResponseServer();
+	}
+}

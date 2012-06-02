fba35def2379332ac5b43d5c8310a4258b291ca7
Graham Weldon <graham@grahamweldon.com>
2012-06-02 21:48:44 +1000

    Added database properties file, and fleshed out the Response server basics

--- RouteMeServer/src/routeme/server/response/ResponseServer.java
+++ RouteMeServer/src/routeme/server/response/ResponseServer.java
@@ -1,13 +1,37 @@
 package routeme.server.response;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ResponseServer {
 
+	public ResponseServer(Properties prop) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Unable to load MySQL Driver");
+			System.exit(1);
+		}
+	}
 	
+	protected void getConnection() {
+		
+	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ResponseServer server = new ResponseServer();
+		Properties prop = new Properties();
+		
+		try {
+			prop.load(new FileInputStream("database.properties"));
+			ResponseServer server = new ResponseServer(prop);
+		} catch (IOException e) {
+			System.out.println("Failed to load database.properties file");
+			System.exit(2);
+		}
+		
 	}
 }

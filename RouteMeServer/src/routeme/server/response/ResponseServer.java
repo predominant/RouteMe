6bef63291f74e0e83b0fe305cd5611bef32df689
Graham Weldon <graham@grahamweldon.com>
2012-06-02 22:18:57 +1000

    Finalised connection getting and added some nice debug messages.

--- RouteMeServer/src/routeme/server/response/ResponseServer.java
+++ RouteMeServer/src/routeme/server/response/ResponseServer.java
@@ -2,15 +2,16 @@ package routeme.server.response;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ResponseServer {
 
 	public ResponseServer() {
-	}
-	
-	public void start(Properties prop) {
 		try {
+			System.out.println("Resolving MySQL driver");
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Unable to load MySQL Driver");
@@ -18,14 +19,38 @@ public class ResponseServer {
 		}
 	}
 	
-	protected void getConnection() {
+	public void start(Properties prop) {
+		Connection conn = this.getConnection(prop);
+	}
+	
+	protected Connection getConnection(Properties prop) {
+		Connection conn = null;
 		
+		try {
+			System.out.println("Attempting MySQL connection");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://" +
+					prop.getProperty("hostname") + "/" +
+					prop.getProperty("database") + "?" +
+					"user=" + prop.getProperty("username") + "&" +
+					"password=" + prop.getProperty("password"));
+			
+		} catch (SQLException e) {
+			System.out.println("Failed to connect to MySQL database");
+			System.exit(3);
+		}
+		
+		return conn;
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("=============================================");
+		System.out.println("Starting RouteMe ResponseServer");
+		System.out.println("=============================================");
+		
 		Properties prop = new Properties();
 		
 		try {

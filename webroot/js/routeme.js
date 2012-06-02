70037c54b9155605a3a0888334e8cde30aa34e30
Graham Weldon <graham@grahamweldon.com>
2012-06-02 16:54:43 +1000

    Restructure RouteMe js

--- webroot/js/routeme.js
+++ webroot/js/routeme.js
@@ -1,28 +1,39 @@
+var RouteMe = {
+
+	map: null,
+	mapId: 'map',
+	places: {
+		canberra: {
+			lat: -35.2828,
+			lon: 149.1314
+		}
+	},
+
+	initialize: function() {
+		var myLatlng = new google.maps.LatLng(this.places.canberra.lat, this.places.canberra.lon);
+		var myOptions = {
+			center: myLatlng,
+			zoom: 12,
+			streetViewControl: false,
+			mapTypeControl: false,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		this.map = new google.maps.Map(document.getElementById(this.mapId), myOptions);
+		this.map.addEventListener('tilesloaded', this.tilesLoaded)
+	},
+
+	tilesLoaded: function() {
+		console.log("Tiles loaded");
+	}
+};
+
+
 $(function() {
 	$('#map').css({
 		width: window.innerWidth - 450,
 		height: window.innerHeight - 180
 	});
-	initialize();
+	RouteMe.initialize();
 });
 
-var map;
-var places = {
-	canberra: {
-		lat: -35.2828,
-		lon: 149.1314
-	}
-};
-
-function initialize() {
-	var myLatlng = new google.maps.LatLng(places.canberra.lat, places.canberra.lon);
-	var myOptions = {
-		center: myLatlng,
-		zoom: 12,
-		streetViewControl: false,
-		mapTypeControl: false,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-
-	map = new google.maps.Map(document.getElementById('map'), myOptions);
-}
